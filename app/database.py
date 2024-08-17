8bc3a879ec7032ae03551c0d597bd39b4f2fa37b
jcedeno <jcedeno.us+infinity@gmail.com>
2024-08-17 17:04:06 -0400

    feat: connnection should be better now

--- app/database.py
+++ app/database.py
@@ -8,8 +8,7 @@ def get_db():
     obtains the db
     """
     if 'db' not in g:
-        client = MongoClient(current_app.config.get(
-            'MONGO_URI', 'mongodb://localhost:27017'))
+        client = MongoClient(current_app.config.get('MONGO_URI'))
         db_name = current_app.config.get('MONGO_DBNAME', 'db')
         g.db = client[db_name]
     return g.db
